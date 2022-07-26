f8c2b5f9e9e54cbf24865668d32cb50282b7f30e
thinker <thinker.amir@gmail.com>
2022-07-26 18:22:05 +0430

    Change Asterisk realtime driver to odbc
    Add sipp service and set cdr realtime

--- asterisk/scripts/initiator.sh
+++ asterisk/scripts/initiator.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 
-# Append mysql configuration into "res_config_mysql.conf" file.
+# Append mysql configuration
 echo " 
-dbhost = ${DB_HOST} 
-dbname = ${DB_DATABASE} 
-dbuser = ${DB_USERNAME} 
-dbpass = ${DB_PASSWORD} 
-dbport = ${DB_PORT} 
-dbcharset = utf8 
-requirements=warn ; or createclose or createchar 
-" >> /etc/asterisk/res_config_mysql.conf
+[mysql_asterisk]
+Description = MySQL Asterisk
+Driver = MariaDB Unicode
+Database = ${DB_DATABASE}
+Server = ${DB_HOST} 
+User = ${DB_USERNAME}
+Password = ${DB_PASSWORD}
+Port = ${DB_PORT}
+" >> /etc/odbc.ini
+
+echo " 
+[mysql_asterisk]
+enabled => yes
+dsn => mysql_asterisk
+username => ${DB_USERNAME}
+password => ${DB_PASSWORD}
+pre-connect => yes
+sanitysql => select 1
+max_connections => 20
+connect_timeout => 5
+negative_connection_cache => 600
+" >> /etc/asterisk/res_odbc.conf
+
+# Run migrations
+sleep 10        # use sleep to ensure that the database is ready
+sh ./custom/scripts/migrate_asterisk_tables.sh
--- asterisk/scripts/migrate_asterisk_tables.sh
+++ asterisk/scripts/migrate_asterisk_tables.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-cd ../../contrib/ast-db-manage
+cd ./contrib/ast-db-manage
 
 ALEMBIC_MYSQL_URL="sqlalchemy.url = mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}"
 
 sed "s|^sqlalchemy.url.*|${ALEMBIC_MYSQL_URL}|" config.ini.sample > config.ini
+sed "s|^sqlalchemy.url.*|${ALEMBIC_MYSQL_URL}|" cdr.ini.sample > cdr.ini
 
 alembic -c config.ini upgrade head
+alembic -c cdr.ini upgrade head
