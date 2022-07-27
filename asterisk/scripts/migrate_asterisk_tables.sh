eac4d376f9883bb43d5545af0068e0ab2a48a1e4
thinker <thinker.amir@gmail.com>
2022-07-27 21:23:10 +0430

    Change the Asterisk service's network to host
    Made this change because the current networking implementation in docker (20.10), built on the aged iptables and docker-proxy, so can not open long range of ports

--- asterisk/scripts/migrate_asterisk_tables.sh
+++ asterisk/scripts/migrate_asterisk_tables.sh
@@ -2,7 +2,7 @@
 
 cd ./contrib/ast-db-manage
 
-ALEMBIC_MYSQL_URL="sqlalchemy.url = mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}"
+ALEMBIC_MYSQL_URL="sqlalchemy.url = mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
 
 sed "s|^sqlalchemy.url.*|${ALEMBIC_MYSQL_URL}|" config.ini.sample > config.ini
 sed "s|^sqlalchemy.url.*|${ALEMBIC_MYSQL_URL}|" cdr.ini.sample > cdr.ini
